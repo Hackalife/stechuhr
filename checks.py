d8270065dd53f3c5ad983f44ef9194e2c7fc2c95
Hackalife <Lukas@gerrlich.org>
2018-03-15 20:10:01 +0100

    Add files via upload

---
+++ checks.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+
+with open('daylogs.txt',mode='r') as logs:
+    levent = logs.readline()
+pass
+
+timedate = datetime.now().strftime(" [%Y-%m-%d %H:%M:%S]")
+
+
+def dialog():
+    print("Log your events \n","last event: \n")
+    print(levent)
+pass
+
+def addevent():
+
+    with open('daylogs.txt') as logdata:
+        data = logdata.read()
+
+
+    logfile = open("daylogs.txt","w")
+    finlog = ("%s %s")%(timedate, name)
+    logfile.write(str(finlog))
+    logfile.write('\n')
+    logfile.write(data)
+    logfile.close()
+
+pass
+
+dialog()
+
+name = input("eventname ?")
+
+
+if name =="j":
+    name ="joylent gegessen"
+    pass
+
+
+
+
+addevent()
